e3596fb2dc228c4b771991ae2a429c9aebd759bd
张锐 <gzzhangrui2014@corp.netease.com>
2016-02-24 16:34:18 +0800

    update db file name in read_mmdb

--- read_mmdb.py
+++ read_mmdb.py
@@ -1,6 +1,6 @@
 import maxminddb
 import json
-reader = maxminddb.open_database('1.mmdb')
+reader = maxminddb.open_database('MaxMind_DB_Writer_perl_create.mmdb')
 result = reader.get('1.0.16.1')
 reader.close()
 print json.dumps(result, indent=2)
--- read_mmdb.rb
+++ read_mmdb.rb
@@ -1,4 +1,4 @@
 require 'maxminddb'
-db = MaxMindDB.new('/tmp/1.mmdb')
+db = MaxMindDB.new('MaxMind_DB_Writer_perl_create.mmdb')
 ret = db.lookup('1.0.16.1')
 print ret.found?
